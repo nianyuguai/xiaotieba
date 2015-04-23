08b4fd1e4ac3d348d0f99b8c98fe7ce3d0db1a17
840311035@qq.com <840311035@qq.com@3199172c-6a15-4fb1-a162-fc9d5c864034>
2015-04-23 11:13:33 +0000

    add urls

--- 1/web/urls.py
+++ 1/web/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^home/$', include('tieba.views.home', name='home')),
+    url(r'^home/', 'tieba.views.home', name='home')),
 )
